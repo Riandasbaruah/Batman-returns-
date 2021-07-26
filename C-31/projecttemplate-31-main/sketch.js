22503e62f17ff688db432004ed1cbebfe73b43db
Riandasbaruah <71764895+Riandasbaruah@users.noreply.github.com>
2021-07-26 20:10:11 +0530

    Add files via upload

---
+++ C-31/projecttemplate-31-main/sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world
+var drops = []
+var maxDrops = 100
+
+function preload(){
+    
+}
+
+function setup(){
+    engine = Engine.create()
+    world = engine.world
+
+    createCanvas(400,700)
+   
+    if(frameCount%150===0){
+        for(var i=0;i<maxDrops;i++){
+            drops.push(new Drops(random(0,400),random(0,400)))
+        }
+    }
+    
+}
+
+function draw(){
+    background(0)
+    Engine.update(engine)
+
+for (var i = 0;i<maxDrops;i++){
+    drops[i].display()
+    drops[i].Update()
+}
+   drawSprites() 
+}   
+
